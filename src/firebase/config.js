c27a45af8e0f1baab870ff3a95ffbf580592cdc8
Farhan <imfarhan0105@gmail.com>
2026-07-17 01:58:07 +0530

    edit-4-security

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -5,13 +5,13 @@ import { getStorage } from 'firebase/storage';
 import { getAnalytics } from 'firebase/analytics';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCzXcyyQenxV0KavUXLaQA21sx4PyjBkQg",
-  authDomain: "ramco-shine.firebaseapp.com",
-  projectId: "ramco-shine",
-  storageBucket: "ramco-shine.firebasestorage.app",
-  messagingSenderId: "630229796934",
-  appId: "1:630229796934:web:4eacb26f73e7c911046247",
-  measurementId: "G-11VZPX8E3Z"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "ramco-shine.firebaseapp.com",
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "ramco-shine",
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "ramco-shine.firebasestorage.app",
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "630229796934",
+  appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:630229796934:web:4eacb26f73e7c911046247",
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID || "G-11VZPX8E3Z"
 };
 
 // Initialize Firebase
